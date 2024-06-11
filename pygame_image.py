dabaee6c293c7240a2fc3b8868465b299d28b063
Yuyadayo <c0a23008b5@edu.teu.ac.jp>
2024-06-11 16:44:33 +0900

    問題8

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -23,9 +25,17 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = 300, 200
-        screen.blit(kk_img, kk_rct)
+
+        key_lst = pg.key.get_pressed() #全キーの押下状態を取得
+        if key_lst[pg.K_UP]: #上矢印が押された場合
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]: #下矢印が押された場合
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]: #左矢印が押された場合
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]: #右矢印が押された場合
+            kk_rct.move_ip(1, 0)
+        screen.blit(kk_img, kk_rct) #kk_imgをkk?rctの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(200)
